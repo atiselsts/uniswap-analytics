6091025a49057f970da54e415d1202b167d94591
Atis Elsts <atis.elsts@gmail.com>
2023-08-08 09:29:24 +0300

    add v2 support

--- get_sandwich_stats.py
+++ get_sandwich_stats.py
@@ -11,12 +11,22 @@ if POOL is None or len(POOL) == 0:
     POOL = "0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640" # USDC/ETH 0.05%
 POOL = POOL.lower()
 
-decimals = 18 # for USDC
+decimals = 6 # for USDC
+
+# change this to get v2 swaps
+VERSION = os.getenv("VERSION")
+try:
+    VERSION = int(VERSION)
+except:
+    VERSION = 3
+if VERSION not in [2, 3]:
+    print("Uniswap v2 or v3 supported")
+    exit(-1)
 
 self_dir = os.path.dirname(os.path.abspath(__file__))
-data_dir = os.path.join(os.path.dirname(os.path.dirname(self_dir)), "data", "uniswap-v3-swaps", YEAR)
+data_dir = os.path.join(os.path.dirname(os.path.dirname(self_dir)), "data", f"uniswap-v{VERSION}-swaps", YEAR)
 
-not_sandwitch = [
+not_sandwich = [
     "0x3fc91a3afd70395cd496c647d5a6cc9d4b2b7fad", # universal router
     "0xef1c6e67703c7bd7107eed8303fbe6ec2554bf6b", # old uni router
     "0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45", # router 2
@@ -46,7 +56,7 @@ def account_for_mev(eth_buyers, eth_sellers, trades):
     sandwichers = set(eth_buyers.keys()).intersection(set(eth_sellers.keys()))
     traders = set(eth_buyers.keys()).symmetric_difference(set(eth_sellers.keys()))
     for address in sandwichers:
-        if address in not_sandwitch:
+        if address in not_sandwich:
             flag = False
         else:
             flag = True
@@ -60,8 +70,24 @@ def classify_trades(trades, data):
     eth_buyers = {}
     eth_sellers = {}
 
-    # amount0: USDC, amount1: ETH
-    for _, block, _, amount0, amount1, address, _ in data:
+    # token0: USDC, token1: ETH
+    for row in data:
+        if VERSION == 2:
+            (_, block, _, amount0_in, amount1_in, amount0_out, amount1_out, address, _) = row
+            amount0_out = int(amount0_out)
+            amount1_out = int(amount1_out)
+            if amount0_out > 0:
+                # selling ETH
+                amount0 = -amount0_out
+            elif amount1_out > 0:
+                # buying ETH
+                amount0 = int(amount0_in)
+            else:
+                amount0 = 0
+        else:
+            (_, block, _, amount0, amount1, address, _) = row
+            amount0 = int(amount0)
+
         if current_block != block:
             current_block = block
             # account for MEV in the block
@@ -69,8 +95,6 @@ def classify_trades(trades, data):
             # clean up state; look only at single-block MEV
             eth_buyers = {}
             eth_sellers = {}
-        amount0 = int(amount0)
-        amount1 = int(amount1)
         if amount0 < 0:
             # selling ETH, account for USDC volume, including the fee
             eth_sellers[address] = eth_sellers.get(address, 0) - amount0
@@ -94,7 +118,7 @@ def main():
     print(f"total USDC volume: {sum(trades.values()) / (10 ** decimals) * 1e-6:.0f} million")
     sandwich_proportion = trades[True] / sum(trades.values())    
     print(f"sandwich volume proportion: {100*sandwich_proportion:.2f}%")
-    print("top sandwch traders by num tx:")
+    print("top sandwich traders by num tx:")
     sandwichers = sorted(list(num_sandwich_tx.items()), key=lambda x: x[1], reverse=True)
     for i in range(min(10, len(sandwichers))):
         print(sandwichers[i])
